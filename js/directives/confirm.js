0d479b10de2e0f3bb0de500e8c7857d0227d2b17
IvoObr <ivo_obr@hotmail.com>
2017-10-16 02:04:35 +0300

    confirm directive customized
    text for the buttons and title for the popup can be given dinamically

--- js/directives/confirm.js
+++ js/directives/confirm.js
@@ -2,7 +2,16 @@ angular.module('toDoListApp')
     .directive('confirm', function() {
         return {
             templateUrl: 'templates/confirm.html',
-            controller: 'todoController',
-            replace: true
+            // controller: 'todoController',
+            replace: true,
+            transclude: true,
+            link: function (scope, element, attrs) {
+
+                scope.buttonsNum = Number(attrs.numberOfButtons);
+                scope.buttonName1 = attrs.buttonOneName;
+                scope.buttonName2 = attrs.buttonTwoName;
+                scope.title = attrs.title;
+            }
         };
     });
+
